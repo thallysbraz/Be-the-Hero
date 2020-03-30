5f2ecf5909cdbf7d910710ae6b7338f4d6e613ab
thallysbraz <thallysbraz3@gmail.com>
2020-03-30 17:38:26 -0300

    criando ong

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const crypto = require("crypto");
 
 const routes = express.Router();
+
+const connection = require("./database/connection");
 
 routes.get("/", async (req, res) => {
   return res.json({ msg: "ok, true" });
 });
 
-routes.get("/user", async (req, res) => {
-  return res.json({ msg: "ok, novo user" });
+routes.post("/ongs", async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body; //recebendo os dados
+
+  //gerando id da ong
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  const ong = await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+  return res.json(ong.id);
 });
 
 module.exports = routes;
