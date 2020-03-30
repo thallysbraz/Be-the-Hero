d6ce99b90e2102cc094e8d525d3fb3696534dd50
thallysbraz <thallysbraz3@gmail.com>
2020-03-30 17:57:58 -0300

    rota post e get das ongs

--- backend/src/Controllers/OngController.js
+++ backend/src/Controllers/OngController.js
@@ -1,4 +1,34 @@
+const crypto = require("crypto");
+const connection = require("../database/connection");
+
 module.exports = {
   //store para criar ong
-  async store(req, res) {}
+  async store(req, res) {
+    try {
+      const { name, email, whatsapp, city, uf } = req.body; //recebendo os dados
+
+      //gerando id da ong
+      const id = crypto.randomBytes(4).toString("HEX");
+
+      await connection("ongs").insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
+      });
+
+      return res.json({ id });
+    } catch (error) {
+      return res.json(error);
+    }
+  },
+
+  //index para listar ongs cadastradas
+  async index(req, res) {
+    const ongs = await connection("ongs").select("*");
+
+    return res.json(ongs);
+  }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,29 +1,11 @@
 const express = require("express");
-const crypto = require("crypto");
-
 const routes = express.Router();
 
-const connection = require("./database/connection");
+//Controllers
+const OngController = require("./Controllers/OngController");
 
-routes.get("/", async (req, res) => {
-  return res.json({ msg: "ok, true" });
-});
-
-routes.post("/ongs", async (req, res) => {
-  const { name, email, whatsapp, city, uf } = req.body; //recebendo os dados
-
-  //gerando id da ong
-  const id = crypto.randomBytes(4).toString("HEX");
-
-  const ong = await connection("ongs").insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf
-  });
-  return res.json(ong.id);
-});
+//Rotas de ongs
+routes.get("/ongs", OngController.index); // Listar todas as ong
+routes.post("/ongs", OngController.store); // Cadastrar uma ong
 
 module.exports = routes;
