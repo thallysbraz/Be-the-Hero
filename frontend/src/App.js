3362e19ea5ca09924b7a2868a7d33cc401e48411
thallysbraz <thallysbraz3@gmail.com>
2020-04-07 14:42:17 -0300

    terminando pagina de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "./global.css";
+import Logon from "./pages/Logon";
 
 //Componente: Função que retorna um HTML
+//Propriedades: "atributos" repassados para os componentes || parametros
+//Estado: Informação contida no componente
 
 function App() {
-  return <h1>Olá Mundo</h1>;
+  return <Logon />;
 }
 
 export default App;
