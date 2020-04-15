c3f27a0cb2ac59e16da2d0154b034ff43d449eb6
thallysbraz <thallysbraz3@gmail.com>
2020-04-15 11:49:22 -0300

    integrando com api e listando casos

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { Feather } from "@expo/vector-icons";
 
+import { Feather } from "@expo/vector-icons";
 import logoImg from "../../assets/logo.png";
 import styles from "./styles";
 
+import api from "../../services/api";
+
 export default function Incidents() {
   const navigation = useNavigation();
-
+  const [incidents, setIncidents] = useState([]);
+  const [total, setTotal] = useState(0);
   function navigateToDetail() {
     navigation.navigate("Detail");
   }
+
+  //funçao de load de incidents
+  async function loadIncidents() {
+    const response = await api.get("/incidents");
+    setIncidents(response.data);
+    setTotal(response.headers["x-total-count"]);
+  }
+  useEffect(() => {
+    loadIncidents();
+  }, []);
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={logoImg} />
         <Text style={styles.headerText}>
-          Total de <Text style={styles.headerTextBold}> 0 casos</Text>.
+          Total de <Text style={styles.headerTextBold}>{total} casos</Text>.
         </Text>
       </View>
 
@@ -28,20 +41,25 @@ export default function Incidents() {
       </Text>
 
       <FlatList
-        data={[1, 2, 3]}
+        data={incidents}
         style={styles.incidentsList}
-        keyExtractor={(incident) => String(incident)}
+        keyExtractor={(incident) => String(incident.id)}
         showsVerticalScrollIndicator={false}
-        renderItem={() => (
+        renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
-            <Text style={styles.incidentValue}>APAD</Text>
+            <Text style={styles.incidentValue}>{incident.name}</Text>
 
             <Text style={styles.incidentProperty}>CASO:</Text>
-            <Text style={styles.incidentValue}>cachorro atropelado</Text>
+            <Text style={styles.incidentValue}>{incident.title}</Text>
 
             <Text style={styles.incidentProperty}>VALOR:</Text>
-            <Text style={styles.incidentValue}>120.50</Text>
+            <Text style={styles.incidentValue}>
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(incident.value)}
+            </Text>
 
             <TouchableOpacity
               style={styles.detailsButton}
