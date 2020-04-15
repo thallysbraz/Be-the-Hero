1880a236dea388061527e5151250fd228b8c7199
thallysbraz <thallysbraz3@gmail.com>
2020-04-15 10:02:04 -0300

    inciando page de detail

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,6 +1,19 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image, Text, TouchableOpacity } from "react-native";
+
+import { Feather } from "@expo/vector-icons";
+import styles from "./styles";
+import logoImg from "../../assets/logo.png";
 
 export default function Detail() {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <TouchableOpacity>
+          <Feather name="arrow-left" size={28} color="#e02041" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
---
+++ mobile/src/pages/Detail/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: "#7159c1",
+  },
+  header: {},
+  headerText: {},
+  headerTextBold: {},
+});
