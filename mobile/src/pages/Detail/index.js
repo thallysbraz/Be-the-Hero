9142c0bd7f04959a5e4644c1c70b04f2d4f1c483
thallysbraz <thallysbraz3@gmail.com>
2020-04-15 10:57:51 -0300

    finalizando style da pagina de detail

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -14,6 +14,32 @@ export default function Detail() {
           <Feather name="arrow-left" size={28} color="#e02041" />
         </TouchableOpacity>
       </View>
+
+      <View style={styles.incident}>
+        <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
+        <Text style={styles.incidentValue}>APAD</Text>
+
+        <Text style={styles.incidentProperty}>CASO:</Text>
+        <Text style={styles.incidentValue}>cachorro atropelado</Text>
+
+        <Text style={styles.incidentProperty}>VALOR:</Text>
+        <Text style={styles.incidentValue}>120.50</Text>
+      </View>
+
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTittle}>Salve o dia!</Text>
+        <Text style={styles.heroTittle}>Seja o herói.</Text>
+        <Text style={styles.heroDescription}>Entre em contato:</Text>
+
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionsText}>WhatsApp</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionsText}>Email</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
