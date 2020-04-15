07ce3927c74d96e1014f470572872f67ee90f56f
thallysbraz <thallysbraz3@gmail.com>
2020-04-15 09:38:39 -0300

    fazendo paginas e arrumando styles

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import Routes from "./src/routes";
+
 export default function App() {
-  return (
-    <View>
-      <Text>Hello!</Text>
-    </View>
-  );
+  return <Routes />;
 }
